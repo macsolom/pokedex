901c4a070938b8b207cf1f228172dfc3d79245ca
macsolom <77767262+macsolom@users.noreply.github.com>
2021-03-04 19:26:29 -0800

    Update script.js according to task 1.2
    Create a variable, pokemonList, and assign data to its array.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,2 @@
-// 1.1 What is JavaScript
-alert('Hello World');
-
-let favoriteFood = 'Apple';
-document.write(favoriteFood);
+// 1.2 JavaScript Basicis pt.1 (Create a variable, pokemonList, and assign data to its array)
+let pokemonList = [{name: "Zapdos", height: 1.6, types: ["electric","flying"]}{name: "Butterfree", height: 1.1, types: ["bug","flying"]}{name: "Onix", height: 8.8, types: ["rock","ground"]}];
