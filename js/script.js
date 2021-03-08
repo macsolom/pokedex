8a7dc9bc7d403bd1e53822afa9f5ecf05d1c31af
macsolom <77767262+macsolom@users.noreply.github.com>
2021-03-07 17:57:29 -0800

    Update script.js according to task 1.3 with for loop
    Added a for loop to iterate over every element in the array and then write it on the website. There's also an if logic that will look at the height of the Pokemon and add extra text depending on the parameters.

--- js/script.js
+++ js/script.js
@@ -16,3 +16,15 @@ let pokemonList = [
     types: ["rock","ground"]
   }
 ];
+
+/* 1.3 A for loop to iterate over every element in the array and then write it on the website. There's also an if logic that will look at the height of the pokemon and add extra text depending on the parameters.
+*/
+for (let i=0; i < pokemonList.length; i++){
+  if (pokemonList[i].height > 2.0 && pokemonList[i].height < 10) {
+    document.write(`${(pokemonList[i].name)} - Height: ${pokemonList[i].height}m - Wow, that's big! <br/> <br/>`);
+  } else if (pokemonList[i].height < 1.0) {
+    document.write(`${(pokemonList[i].name)} - Height: ${pokemonList[i].height}m - This is a small Pokemon! <br/> <br/>`);
+  } else {
+    document.write(`${(pokemonList[i].name)} - Height: ${pokemonList[i].height}m <br/> <br/>`);
+  }
+};
