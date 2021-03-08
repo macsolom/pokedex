16b7702d6ad1bee699979ead2c2c5c169eb62570
macsolom <77767262+macsolom@users.noreply.github.com>
2021-03-07 17:10:57 -0800

    Updated script.js array arrangement
    Arranged pokemonList array for better readability.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 // 1.2 JavaScript Basicis pt.1 (Create a variable, pokemonList, and assign data to its array)
-let pokemonList = [{name: "Zapdos", height: 1.6, types: ["electric","flying"]},{name: "Butterfree", height: 1.1, types: ["bug","flying"]},{name: "Onix", height: 8.8, types: ["rock","ground"]}];
+let pokemonList = [
+  {
+    name: "Zapdos",
+    height: 1.6,
+    types: ["electric","flying"]
+  },
+  {
+    name: "Butterfree",
+    height: 1.1,
+    types: ["bug","flying"]
+  },
+  {
+    name: "Onix",
+    height: 8.8,
+    types: ["rock","ground"]
+  }
+];
