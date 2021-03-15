bb5bf5f99c4478fc2dab489358fe9c750b52835a
macsolom <cmhorton8@icloud.com>
2021-03-15 15:15:30 -0700

    Updated script.js according to task 1.5
    -Created new pokemonRepository variable to hold IIFE.
    -Replace for loop with forEach loop

--- js/script.js
+++ js/script.js
@@ -1,30 +1,50 @@
 // 1.2 JavaScript Basicis pt.1 (Create a variable, pokemonList, and assign data to its array)
-let pokemonList = [
-  {
-    name: "Zapdos",
-    height: 1.6,
-    types: ["electric","flying"]
-  },
-  {
-    name: "Butterfree",
-    height: 1.1,
-    types: ["bug","flying"]
-  },
-  {
-    name: "Onix",
-    height: 8.8,
-    types: ["rock","ground"]
+let pokemonRepository = (function()){
+  let pokedex = [
+    {
+      name: 'Zapdos',
+      height: 1.6,
+      types: ['electric','flying']
+    },
+    {
+      name: 'Butterfree',
+      height: 1.1,
+      types: ['bug','flying']
+    },
+    {
+      name: 'Onix',
+      height: 8.8,
+      types: ['rock','ground']
+    }
+  ];
+  // Adds pokemon to pokedex - contains a datatype check
+  function add(pokeomon) {
+    if (typeof pokemon === 'object' && typeof pokemon !== null) {
+      pokedex.push(pokemon);
+    } else {
+      console.log('you need an object');
+    }
   }
-];
+  // Function to allow the retrival of the pokedex datatype
+  function getAll() {
+    return pokedex;
+  }
 
-/* 1.3 A for loop to iterate over every element in the array and then write it on the website. There's also an if logic that will look at the height of the pokemon and add extra text depending on the parameters.
-*/
-for (let i=0; i < pokemonList.length; i++){
-  if (pokemonList[i].height > 2.0 && pokemonList[i].height < 10) {
-    document.write(`${(pokemonList[i].name)} - Height: ${pokemonList[i].height}m - Wow, that's big! <br/> <br/>`);
-  } else if (pokemonList[i].height < 1.0) {
-    document.write(`${(pokemonList[i].name)} - Height: ${pokemonList[i].height}m - This is a small Pokemon! <br/> <br/>`);
+  return {
+    addF: add,
+    getAllF: getAll
+  };
+})();
+
+pokemonRepository.addF({name: 'Jynx', height: 1.4, types: ['Psychic', 'Ice']});
+
+//Using the .forEach method, this is iterating over the elements in the pokedex array and deciding based on height parameters how to write them out
+pokemonRepository.getALLF().forEach(function(pokemon) {
+  if (pokemon.height > 2.0 && pokemon.height < 5) {
+    document.write(`${(pokemon.name)} - Height: ${pokemon.height}m - Wow, that's big! <br/> <br/>`);
+  } else if (pokemon.height < 1.0) {
+    document.write(`${(pokemon.name)} - Height: ${pokemon.height}m - This is a small Pokemon! <br/> <br/>`);
   } else {
-    document.write(`${(pokemonList[i].name)} - Height: ${pokemonList[i].height}m <br/> <br/>`);
+    document.write(`${(pokemon.name)} - Height: ${pokemon.height}m <br/> <br/>`);
   }
 };
